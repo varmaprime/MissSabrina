52571b5989dbbb23641c89459db6f351d9942ca2
varmaprime <64773606+varmaprime@users.noreply.github.com>
2020-05-04 14:50:36 +0530

    Update __main__.py
    New 05

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,19 +27,9 @@ def escape_html(word):
     return escape(word)
 
 PM_START_TEXT = """
-Hello *{}* My name is *Sabrina*!
+Hey there! My name is MissRosexXx - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.
 
-I'm here to manage your groups! 
-
-Click Help button to find out more about how to use me to my full potential. 
-
-Join Support Chat ( @SabrinaChat ) if you need any support or help 
-
-Join ( @SabrinaOfficial ) if you want to keep up with the news, updates and bot downtime!
-
-Made by @TechnoAyanOfficial
-
-Want me to help you? [Click Here!](https://t.me/MissSabrina_bot?startgroup=true)
+Want to add me to your chat? [Click here!](http://telegram.me/MissRose_xXx_Bot?startgroup=botstart) 
 """
 
 
@@ -47,21 +37,26 @@ Want me to help you? [Click Here!](https://t.me/MissSabrina_bot?startgroup=true)
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
+Hey! My name is MissRosexXx . I am a group management bot, here to help you get around and keep the order in your groups!
+I have lots of handy features, such as flood control, a warning system, a note keeping system, and even predetermined replies on certain keywords.
 
-*Main* available commands:
- - /start: Start the bot...
- - /help: help....
- - /donate: To find out more about donating!
- - /settings:
-   - in PM: To find out what SETTINGS you have set....
-   - in a group:
+Helpful commands:
+- /start: Starts me! You've probably already used this.
+- /help: Sends this message; I'll tell you more about myself!
+- /donate: Gives you info on how to support me and my creator.
+
+If you have any bugs or questions on how to use me, here you can ask [My Creator](t.me/Unknown_Hacker_ZC) , or head to @MissRose_xXx_Bot_Support.
+ All commands can be used with the following: / !
 
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
-DONATE_STRING = """Hey you can Donate to [Ayan Ansari](t.me/TechnoAyanOfficial) for better server """
+DONATE_STRING = """So you want to donate? Amazing!
+It took a lot of work for my creator to get me to where I am now - so if you have some money to spare, and want to show your support; Donate!
+After all, server fees don't pay themselves - so every little helps! All donation money goes straight to funding the VPS, and of course, boosting morale - always nice to see my work is appreciated :)
+You can donate to [My Creator](t.me/Unknown_Hacker_ZC) , or if you want to help support me on a monthly basis, you can set up a recurring donation on [GitHub Sponsors](t.me/unknown_hacker_x) .
+Thank you for your generosity! """
 
 
 IMPORTED = {}
@@ -153,7 +148,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("Yo, I'm alive👩")
+        update.effective_message.reply_text("Heya :) PM me if you have any questions on how to use me!")
 
 def send_start(bot, update):
     #Try to remove old message
@@ -169,10 +164,10 @@ def send_start(bot, update):
                 
     keyboard = [[
         InlineKeyboardButton(text=tld(chat.id, '🥳Add To Group'),
-                             url="https://t.me/MissSabrina_bot?startgroup=true"), InlineKeyboardButton(text="❓ Help", callback_data="help_back")
+                             url="https://t.me/MissRose_xXx_Bot?startgroup=true"), InlineKeyboardButton(text="❓ Help", callback_data="help_back")
         ]]
     keyboard += [[
-        InlineKeyboardButton(text=tld(chat.id, '👥 Support Group'), url="https://t.me/SabrinaChat")
+        InlineKeyboardButton(text=tld(chat.id, '👥 Support Group'), url="https://t.me/MissRose_xXx_Bot_Support")
     ]]
 
     update.effective_message.reply_text(
