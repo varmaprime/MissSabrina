43b20572afa90f0a2536848cc07fee1b0d8c7249
varmaprime <64773606+varmaprime@users.noreply.github.com>
2020-05-04 14:15:44 +0530

    Update welcome.py
    New 01

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -92,7 +92,7 @@ def new_member(bot: Bot, update: Update):
         for new_mem in new_members:
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("My Master Is Here dude,Woah🤗!")
+                update.effective_message.reply_text("Yay! My creator joined the Group.. Give a warm welcome🎉")
                 continue
 
             # Give start information when add bot to group
@@ -102,7 +102,7 @@ def new_member(bot: Bot, update: Update):
                     "I have been added to this chat - {} with \nID: <pre>{}</pre>".format(chat.title, chat.id),
                     parse_mode=ParseMode.HTML
                 )
-                update.effective_message.reply_text("Thanks for adding me Sweetheart,CHECK @ctrlsupport!")
+                update.effective_message.reply_text("Thanks for adding me Sweetheart,If you have any doubts or if you need any help please head to @MissRose_xXx_Bot_Support")
 
             else:
                 # If welcome message is media, send with appropriate function
